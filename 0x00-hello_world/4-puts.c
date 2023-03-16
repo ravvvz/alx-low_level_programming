9a0b9ba323288909350684c7aec7303d6033779b
Donald <you@example.com>
2023-03-16 19:31:09 +0100

    C files that prints the string "with proper grammar, but the outcome is a piece of art,"

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
