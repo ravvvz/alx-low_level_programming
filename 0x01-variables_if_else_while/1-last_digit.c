212fb1ab76be9e9ae3f77efbbc3e1e2ad686908c
Donald <you@example.com>
2023-03-17 17:40:20 +0100

    C script that assigns a randome number to a variable "n" and checks if the last digit is greater than 5, is 0, or less than 6 and not 0

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - checks a number an prints the last digit
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is", n);
+	if (n < 0)
+		n = (-n);
+	last_digit = n % 10;
+	printf(" %d ", last_digit);
+	if (last_digit > 5)
+		printf("and is greater than 5\n");
+	else if (last_digit == 0)
+		printf("and is 0\n");
+	else if (last_digit < 6 && last_digit != 0)
+		printf("and is less than 6 and not 0\n");
+	return (0);
+}
