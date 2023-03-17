102acb9a97d81d568ae5d64a8862e9c787f216f8
Donald <you@example.com>
2023-03-17 17:25:17 +0100

    C script that prints a random number and checks if it is positive, zero or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints and checks if a number is positive, zero, or negative
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d", n);
+	if (n > 0)
+		printf(" is positive\n");
+	else if (n == 0)
+		printf(" is zero\n");
+	else if (n < 0)
+		printf(" is negative\n");
+	return (0);
+}
