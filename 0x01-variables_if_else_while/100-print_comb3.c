e9def117cfffaf66489b6f04debc8d1a7a9050d1
Donald <you@example.com>
2023-03-17 19:27:47 +0100

    C script that prints a combo of 2 single digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints the conbination of 2 digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 48; n1 <= 57; n1++)
+	{
+		for (n2 = 48; n2 <= 57; n2++)
+		{
+			if (n2 > n1)
+			{
+				putchar(n1);
+				putchar(n2);
+				if (n1 != 56 && n2 != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
