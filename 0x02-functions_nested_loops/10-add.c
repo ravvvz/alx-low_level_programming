2b62bd60a744c318b25e24d385070bec145c8423
Donald <you@example.com>
2023-03-21 23:28:00 +0100

    C script that adds two integers together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two int together
+ * @n1: the first int to add
+ * @n2: the second int to add
+ *
+ * Return: result of the addition
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
