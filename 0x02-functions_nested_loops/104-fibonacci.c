00fbeb54960cb137bd8405ae3050271ca26f0c17
Donald <you@example.com>
2023-03-22 00:42:37 +0100

    C script that prints the first fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print the first 100 fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long int a, b, c, d;
+
+	a = 1, b = 2;
+
+	for (d = 0; d < 100; d++)
+	{
+		printf("%lu", a);
+
+		if (!(d == 100))
+			printf(", ");
+
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	putchar('\n');
+}
