a470f9326e18a1f42e2a6dc9c08f2e65e0557438
Donald <you@example.com>
2023-03-22 00:01:04 +0100

    C script that prints the sum of all natural numbers divisible by 3 and 5 from 0 to 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all natural numbers
+ *
+ * Return: int
+ */
+int main(void)
+{
+	int sum = 0, n;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			sum += n;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
