9dd689b673625c72f694c68dea036e7b9c655621
Donald <you@example.com>
2023-03-21 21:29:40 +0100

    C script that checks if param is alpha or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if the param is alpha or not
+ * @c: accepts a param of type int
+ *
+ * Return: 1 if letter is lower or alpha, 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122 )|| (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+
+	return (0);
+}
