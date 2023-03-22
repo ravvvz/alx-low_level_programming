c2e3940d62b498789299f8b2eae9217956e7cd56
Donald <you@example.com>
2023-03-22 02:00:00 +0100

    C script that computes the time in a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,24 +7,18 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int a, b;
 
-	for (a = 0; a <= 2; a++)
+	for (a = 0; a < 24; a++)
 	{
-		for (b = 0; b <= 3; b++)
+		for (b = 0; b < 60; b++)
 		{
-			for (c = 0; c <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(':');
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
