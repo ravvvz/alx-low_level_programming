7fb6241ece90bfd72869794fc9928bdffb8d03e7
Donald <you@example.com>
2023-03-23 20:21:14 +0100

    C script that print fixx for every number divisible by 3 and buzz for that by 5 and fizzbuzz for those by 3 and 5

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - prints fizzbuzz
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 != 0))
+		{
+			printf(" Fizz ");
+		}
+		else if ((a % 5 == 0) && (a % 3 != 0))
+		{
+			printf(" Buzz ");
+		}
+		else if ((a % 5 == 0) && (a % 3 == 0))
+		{
+			printf(" FizzBuzz ");
+		}
+		else
+			printf(" %d ", a);
+	}
+	printf("\n");
+}
