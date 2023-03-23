5799079800954c736354e20df9f19e5fa1ff5a27
Donald <you@example.com>
2023-03-23 18:56:12 +0100

    C function that prints the digit 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers 0-9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+		_putchar(a++);
+
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,8 +2,12 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif /* MAIN_H */
