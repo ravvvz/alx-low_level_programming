d8f5e8c18a951f9e8536aed428d9ffa7d894da93
Donald <you@example.com>
2023-03-25 22:17:46 +0100

    C file that prints a triangle followed by a new line

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle with "#"
+ * @size: size of the triangle (datatype: int)
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int a, b, x;
+
+	x = size;
+	for (a = 1; a <= size; a++)
+	{
+		for (b = 1; b <= size; b++)
+		{
+			if (b >= x) /* conflicting condition */
+			{
+				_putchar('#');
+			}
+			else
+				_putchar(' ');
+		}
+		x--;
+		_putchar('\n');
+	}
+}
