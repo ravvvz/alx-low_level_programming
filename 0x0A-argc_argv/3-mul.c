331659abbc6573a7b80e3e1f5022c7358ee731e6
Donald <you@example.com>
2023-04-07 11:51:11 +0100

    C function that multiplies two int passed to it

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int _atoi(char *s, int *status);
+/**
+ * main - multiplies two int arguments
+ * @argc: the counter of arguments
+ * @argv: array of pointer to strings of the arguments
+ *
+ * Return: 1 if successful and 0 if not
+ */
+
+int main(int argc, char **argv)
+{
+	int mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int num1, num2, status0 = 1, status1 = 1;
+
+		num1 = _atoi(argv[1], &status0);
+		num2 = _atoi(argv[2], &status1);
+
+		if (status0 == -1 || status1 == -1)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			mul = num1 * num2;
+	}
+	printf ("%d\n", mul);
+
+	return (0);
+}
+
+
+/**
+ * _atoi - converts a string to integer
+ * @s: the string to convert
+ *
+ * Return: integer conversion of string
+ */
+
+int _atoi(char *s, int *status)
+{
+	int i, num = 0;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] < '0' || s[i] > '9')
+		{
+			*status = -1;
+			return (0);
+		}
+		i++;
+	}
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[0] == '-')
+			i++;
+		num = (num * 10) + (s[i] - '0');
+		i++;
+	}
+	
+	if (s[0] == '-')
+		num *= -1;
+
+	return (num);
+}
