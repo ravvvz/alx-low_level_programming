d53fbd1f1447eb6b799749d1bb3fb9250f590176
Donald <you@example.com>
2023-04-08 22:46:00 +0100

    C function that adds positive numbers from arguments pass in CLI

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int _atoi(char *s, int *status);
 /**
  * main - prints the sum of all positive numbers
  * @argc: the number of strings
@@ -20,69 +21,22 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		int i, num, status = 1;
+		int i, j;
 
 		for (i = 1; i < argc; i++)
 		{
-			num = _atoi(argv[i], &status);
-			if (status == -1)
+
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			else
-				sum += num;
+			sum += atoi(argv[i]);
 		}
 	}
 	printf("%d\n", sum);
 	return (0);
-}
-
-/**
- * _atoi - convert char to int
- * @s: the string to convert
- * @status: if value if positive or negative
- *
- * Return: total of the addition
- */
-
-int _atoi(char *s, int *status)
-{
-	int i, num = 0;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] < '0' || s[i] > '9')
-		{
-			if (i == 0 && s[i] == '-')
-			{
-				i++;
-				continue;
-			}
-			else
-			{
-				*status = -1;
-				return (1);
-			}
-		}
-		i++;
-	}
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (i == 0 && s[i] == '-')
-		{
-			i++;
-			continue;
-		}
-
-		num = (num * 10) + (s[i] - '0');
-		i++;
-	}
-	if (s[0] == '-')
-	{
-		num *= -1;
-	}
-	return (num);
 }
