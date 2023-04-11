aa59ecda4345e58367010d5d0b1dfb2ccb2f035d
Donald <you@example.com>
2023-04-11 18:48:18 +0100

    C function that duplicates a string passed to it as an argument and returns the copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicates a strings and returns the duplicate
+ * @str: the string to duplicate
+ *
+ * Return: the pointer to the string
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int size, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	size = 0;
+	while (str[size] != '\0')
+		size++;
+
+	s = malloc(sizeof(char) * size);
+	if (s == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+		s[i] = str[i];
+		i++;
+	}
+
+	s[i] = '\0';
+
+	return (s);
+}
