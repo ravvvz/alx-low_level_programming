9f0af55f37d8690edc108429faebb5176d1eb1ea
Donald <you@example.com>
2023-04-11 19:14:02 +0100

    C function that concatenates two strings passed to it as argument and returns the pointer to the concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,10 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	int i, j = 0, v = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	while (s1[j] != '\0')
 		j++;
