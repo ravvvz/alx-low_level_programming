459385bc051c74b4a926fa06007fea871a221790
Donald <you@example.com>
2023-04-11 18:28:46 +0100

    C function that creates memory for an array in heap

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	str = malloc((sizeof(char) * size) + 1);
+	str = malloc(sizeof(char) * size);
 	if (str == NULL)
 		return (NULL);
 
