a5ed8391cb644fc42a8739ce6d074451883289ba
Donald <you@example.com>
2023-04-12 00:57:44 +0100

    C function that stores the argument passed in CLI into malloc

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * argstostr - receives argument from CLI and prints it
+ * @ac: number of arguments passed to the program
+ * @av: the string to the arg
+ *
+ * Return: char *
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, k, j, len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			len++;
+		}
+		len++;
+	}
+
+	str = malloc(sizeof(char) * len);
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0, j = 0; i < ac || j < len; i++)
+	{
+		for (k = 0; av[i][k] != '\0'; k++)
+		{
+			str[j] = av[i][k];
+			j++;
+		}
+		str[j] = '\n';
+		j++;
+	}
+
+	return (str);
+}
