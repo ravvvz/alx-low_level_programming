8c49affa897037c3d42ae232399bcab5627d5349
Donald <you@example.com>
2023-04-18 00:49:22 +0100

    C function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,40 +16,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = _strdup(name);
+	dog->name = strdup(name);
 	dog->age = age;
-	dog->owner = _strdup(owner);
+	dog->owner = strdup(owner);
 
 	return (dog);
-}
-
-/**
- * _strdup - copies a string
- * @s: the string
- *
- * Return: returns a copy of the string
- */
-
-char *_strdup(char *s)
-{
-	char *copy;
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-
-	copy = malloc(sizeof(char) * i);
-	if (copy == NULL)
-		return (NULL);
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		copy[i] = s[i];
-		i++;
-	}
-
-	return (copy);
-
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dog - new type to create a dog
@@ -27,6 +28,5 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-char *_strdup(char *s);
 
 #endif /* DOG_H */
