f32ac82cf05bde06db47a42b87c1537115a1041b
Donald <you@example.com>
2023-04-17 23:53:58 +0100

    C header file that defines a new type using struct for a dog attribute

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - new type to create a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: To create a dog attribute
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	int age;
+};
+
+#endif /* DOG_H */
