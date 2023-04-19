16702e5d411028eb82cd63636f40c35180bec7ce
Donald <you@example.com>
2023-04-19 20:28:58 +0100

    C function that prints a name as is and executes a function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name as is
+ * @name: the name as string
+ * @f: function pointer to code
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
