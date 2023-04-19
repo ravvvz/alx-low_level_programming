b653cb87042984d904f12930e781b9ef9d652a18
Donald <you@example.com>
2023-04-19 20:34:22 +0100

    C function that prints the name as is using a function param

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints the name as is
@@ -11,6 +12,6 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL)
-		return;
+		exit(98);
 	f(name);
 }
