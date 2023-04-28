73e019a9871f4c22deaa57b4a674023eace8a33c
Donald <you@example.com>
2023-04-28 15:11:35 +0100

    C function that pops the first element off the linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pop the first element off the list
+ * @head: the head of the list
+ *
+ * Return: the element
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int n;
+
+	tmp = *head;
+	n = (*head)->n;
+	*head = (*head)->next;
+	free(tmp);
+
+	return (n);
+}
