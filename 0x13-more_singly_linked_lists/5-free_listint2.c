4fee76a56c177b202d4b836a682cee31cc30febb
Donald <you@example.com>
2023-04-29 14:10:48 +0100

    C function that frees a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: the head of the link
+ *
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *h;
+
+	h = *head;
+	while (h != NULL)
+	{
+		tmp = h;
+		h = h->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
