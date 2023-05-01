6d6d2d36ad1c43313e6b9272a59233ff8c34a72d
Donald <you@example.com>
2023-05-01 15:50:09 +0100

    C function that inserts a new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a node at an index
- * @head: the head of the list
- * @idx: the index to add it at
+ * insert_nodeint_at_index - inserts a node at a pos
+ * @head: the head of the linked list
+ * @idx: the index to add the node at
  * @n: the element to add
  *
- * Return: head to linked list
+ * Return: the head to the list
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -14,36 +14,33 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new = NULL, *h = NULL, *prev = NULL;
 	unsigned int i;
 
-	i = 0;
-	h = *head;
+	if (*head == NULL || head == NULL)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (*head);
+	}
+	h = *head;
+	i = 0;
 	while (h != NULL)
 	{
-		if (idx == 0)
-		{
-			new->next = *head;
-			*head = new;
-			return (*head);
-		}
 		if (i == idx)
 		{
 			new->next = h;
 			prev->next = new;
 			return (*head);
 		}
-/*		if ((i + 1) == idx) */
-/*		{ */
-/*			new->next = NULL; */
-/*			h->next = new; */
-/*			return (*head); */
-/*		}*/
 		prev = h;
 		h = h->next;
 		i++;
 	}
+
 	return (NULL);
 }
