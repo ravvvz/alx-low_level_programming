b2299951cdea8245e63446512fd516e1e2b9b532
Donald <you@example.com>
2023-05-02 14:38:12 +0100

    C function that reverses a string

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,6 +11,8 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev, *next;
 
+	if (head == NULL || *head == NULL)
+		return (NULL);
 	prev = NULL;
 	next = NULL;
 	while ((*head)->next != NULL)
