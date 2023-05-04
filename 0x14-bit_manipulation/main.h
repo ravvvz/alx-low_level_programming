44d959fc52c44cc2bf192913ff13824611a4d2b0
Donald <you@example.com>
2023-05-04 15:44:12 +0100

    main header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
