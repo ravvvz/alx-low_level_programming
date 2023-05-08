da60f0b9f402193ab9c521b2cddf3689698c3e61
Donald <you@example.com>
2023-05-08 20:25:22 +0100

    C function that creates a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buffer[1024];
+	char *buffer;
 	int fd;
 	size_t fd_check = 0, bytes_written = 0, error = -1;
 
@@ -21,16 +21,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
+	buffer = malloc(sizeof(char) * letters + 1);
+	if (buffer == NULL)
+		return (0);
 	fd_check = read(fd, buffer, letters);
 	if (fd_check == error)
+	{
+		free(buffer);
 		return (0);
+	}
 
 	if (fd_check < letters)
 		letters = fd_check;
 	bytes_written = write(STDOUT_FILENO, buffer, fd_check);
 	if (bytes_written == error)
+	{
+		free(buffer);
 		return (0);
+	}
 
 	close(fd);
+	free(buffer);
 	return (bytes_written);
 }
